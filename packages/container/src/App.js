3bf33f6339d68c9e926f79fa72f87bd34bbf40b2
lungso <dragonsls.so@gmail.com>
2022-11-30 15:53:41 +0800

    randomize production chmassnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import {
+    StylesProvider, 
+    createGenerateClassName
+} from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 //import { mount } from 'marketing/MarketingApp'
@@ -8,15 +12,19 @@ import Header from './components/Header';
 //testing2 
 //testing3
 
-
+const generateClassNmae = createGenerateClassName({
+    productionPrefix: 'co',
+});
 
 export default () => {
     return (
         <BrowserRouter>
+            <StylesProvider generateClassName={generateClassNmae}>
             <div>
                 <Header/>
                 <MarketingApp/>
             </div>
+            </StylesProvider>
         </BrowserRouter>
     );
 };
